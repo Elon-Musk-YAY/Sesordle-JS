eb98305dddcdb7c0b74b691f526c1666f7a02b47
Elon-Musk-YAY <91766805+Elon-Musk-YAY@users.noreply.github.com>
2022-05-02 21:36:01 -0400

    first

---
+++ src/sesordle.js
@@ -0,0 +1,148 @@
+
+const alertContainer = document.querySelector("[data-alert-container]")
+const guess_grid = document.querySelector("[data-guess-grid]")
+const targetWords = ["tromie"]
+var guess_count = 1;
+
+const offsetFromDate = new Date(2022,4,2)
+const msOffset = Date.now() - offsetFromDate
+const dayOffset = msOffset / 1000 / 60 / 60 / 24
+const targetWord = targetWords[Math.floor(dayOffset)]
+
+
+
+function startInteraction () {
+    document.addEventListener("keydown", handleKeyPress);
+  }
+  
+function pressKey(key) {
+    const activeTiles = getActiveTiles();
+    if (activeTiles.length >= 6) return
+    const next = guess_grid.querySelector(":not([data-letter])")
+    next.dataset.letter = key.toLowerCase()
+    next.textContent = key
+    next.dataset.state = "active"
+}
+  
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      submitGuess();
+      return;
+    }
+    if (e.key === "Backspace" || e.key === "Delete") {
+      deleteKey();
+      return;
+    }
+    if (e.key.match(/^[a-z]$/)) {
+      pressKey(e.key);
+      return;
+    }
+  
+}
+
+  function getActiveTiles() {
+    return guess_grid.querySelectorAll("[data-state='active']")
+}
+  
+  function stopInteraction () {
+    document.removeEventListener("keydown", handleKeyPress);
+}
+
+function deleteKey () {
+    const activeTiles = getActiveTiles();
+    const lastTile = activeTiles[activeTiles.length - 1];
+    if (lastTile == null) return;
+    lastTile.textContent = "";
+    delete lastTile.dataset.letter;
+    delete lastTile.dataset.state;
+}
+
+function submitGuess () {
+    const activeTiles = [...getActiveTiles()]
+    if (activeTiles.length < 6) {
+     showAlert("Not enough letters")
+     shakeTiles(activeTiles)
+    return
+    }
+    const guess = activeTiles.reduce((word, tile) => {
+        return word + tile.dataset.letter
+    }, "")
+    console.log(guess)
+    stopInteraction()
+    activeTiles.forEach((...params) => flipTiles(...params, guess))
+    guess_count++;
+    
+}
+
+function showAlert(message, duration = 1000) {
+    const alert = document.createElement("div");
+    alert.textContent = message;
+    alert.classList.add("alert");
+    alertContainer.prepend(alert);
+    if (duration == null) return 
+    setTimeout(() => {
+        alert.classList.add("hide")
+        alert.addEventListener("transitionend", () => {
+            alert.remove()
+        });
+    }, duration);
+    
+} 
+
+function shakeTiles(tiles) {
+    tiles.forEach(tile => {
+        tile.classList.add("shake")
+        tile.addEventListener("animationend", () => {
+            tile.classList.remove("shake")
+        }, {once: true});
+    });
+}
+
+function flipTiles (tile, index, array, guess) {
+    const letter = tile.dataset.letter;
+    setTimeout(() => {
+        tile.classList.add("flip")
+    }, (index*500)/2);
+
+    tile.addEventListener("transitionend", () => {
+        tile.classList.remove("flip")
+        if (targetWord[index] === letter) {
+            tile.dataset.state = "correct"
+        } else if (targetWord.includes(letter)) {
+            tile.dataset.state = "wrong-location"
+        } else {
+            tile.dataset.state = "wrong"
+        }
+
+        if (index === array.length - 1) {
+            tile.addEventListener("transitionend", () => {
+                startInteraction()
+                checkWinLose(guess,array);
+            }, {once: true});
+        }
+    },  {once: true});
+
+}
+
+function checkWinLose(guess, tiles) {
+    if (guess == targetWord) {
+        showAlert("You win!",5000)
+        danceTiles(tiles)
+        stopInteraction()
+        return
+    }
+}
+
+function danceTiles(tiles) {
+    tiles.forEach((tile, index) => {
+        setTimeout(() => {
+        tile.classList.add("dance")
+        tile.addEventListener("animationend", () => {
+            tile.classList.remove("dance")
+        }, {once: true});
+        }, (index*500)/5);
+    });
+}
+
+
+startInteraction();
